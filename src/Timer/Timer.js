c183547520c4b198c28a57615825a4f1f0527b83
serii <seriiburduja@gmail.com>
2021-02-23 21:02:16 +0200

    finished timer

--- src/Timer/Timer.js
+++ src/Timer/Timer.js
@@ -3,6 +3,7 @@ import styles from './Timer.module.css';
 
 class Timer extends Component {
   state = {
+    buttonText: 'Start',
     timer: 0,
     walk: false
   }
@@ -10,26 +11,22 @@ class Timer extends Component {
     if (!this.state.walk) {
       this.timerId = setInterval(() => {
         this.setState({timer: this.state.timer + 1})
-        if (!localStorage.getItem('timer')) {
-          localStorage.setItem('timer', this.state.timer)
-        }
-        clearInterval(this.timerId)
         this.setState({walk: true})
+        this.setState({buttonText: 'Stop'})
       }, 1000)
     } else {
       clearInterval(this.timerId)
       this.setState({walk: false})
+      this.setState({buttonText: 'Start'})
     }
   }
   reset = () => {
     this.setState({walk: false, timer: 0})
     clearInterval(this.timerId)
-    if (!localStorage.getItem('timer')) {
-      localStorage.setItem('timer', this.state.timer)
-    }
   }
   
   componentDidMount () {
+    this.setState({buttonText: 'Start'})
     if (!localStorage.getItem('timer')) {
       localStorage.setItem('timer', this.state.timer)
     } else {
@@ -38,13 +35,21 @@ class Timer extends Component {
     }
   }
   
+  componentDidUpdate () {
+    localStorage.setItem('timer', this.state.timer)
+  }
+  componentWillUnmount () {
+    clearInterval(this.timerId)
+    localStorage.setItem('timer', this.state.timer)
+  }
+  
   render () {
     return (
       <div className={styles.timer}>
         <h3>React timer</h3>
         <p>{this.state.timer}</p>
         <p>
-          <button onClick={this.start}>Start</button>
+          <button onClick={this.start}>{this.state.buttonText}</button>
           <button onClick={this.reset}>Reset</button>
         </p>
       </div>
